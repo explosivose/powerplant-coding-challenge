224299756a7277a56dec4ce7c81acdb437ce356d
Matt Blickem <explosivose@gmail.com>
2023-12-15 15:26:50 +0100

    wip: power planner

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
 from enum import Enum
 
-class Fuels:
-  gasPrice: float
-  kerosinePrice: float
-  co2Price: float
-  windPc: float
+WIND_PC = 'wind(%)'
+
+class FuelPriceType(Enum):
+  GAS_PRICE = 'gas(euro/MWh)'
+  KEROSINE_PRICE = 'gas(euro/MWh)'
+  CO2_PRICE = 'co2(euro/ton)'
 
 class PowerPlantType(Enum):
   GAS = 'gasfired'
@@ -14,7 +15,7 @@ class PowerPlantType(Enum):
 
 class PowerPlant:
   name: str
-  t: PowerPlantType
+  type: PowerPlantType
   efficiency: float
   pmin: float
   pmax: float
@@ -25,6 +26,38 @@ def create_app():
   @app.post("/productionplan")
   def production_plan_controller():
     d = request.json
-    return d
+    load: float = d['load']
+    fuels: Fuels = d['fuels']
+    plants: list[PowerPlant] = d['powerplants']
+    wind_pc = fuels[WIND_PC] / 100
+
+    merit_order = [PowerPlantType.WIND]
+    if fuels[FuelPriceType.KEROSINE_PRICE.value] > fuels[FuelPriceType.GAS_PRICE.value]:
+      merit_order.append(PowerPlantType.JET)
+      merit_order.append(PowerPlantType.GAS)
+    else:
+      merit_order.append(PowerPlantType.GAS)
+      merit_order.append(PowerPlantType.JET)
+    
+    load_remaining = load
+    plants_used = []
+    print(merit_order)
+    for power_type in merit_order:
+      plant_selection = [x for x in plants if x['type'] == power_type.value]
+      for plant in plant_selection:
+        plants_used.append(plant_selection)
+        if (power_type == PowerPlantType.WIND.value):
+          load_remaining -= plant['pmax'] * wind_pc
+        else:
+          load_remaining -= plant['pmax'] * plant['efficiency']
+        print(load_remaining)
+        if load_remaining < 0:
+          break
+      if load_remaining < 0:
+        break
+
+
+    print(plants_used, '\n')
+    return plants_used
 
   return app
--- test/test_productionplan.py
+++ test/test_productionplan.py
@@ -4,6 +4,5 @@ import json
 def test_productionplan(client):
   with open('example_payloads/payload1.json', 'r') as payload_file:
     response = client.post("/productionplan", json=json.load(payload_file))
-    print(response.data)
     assert b'powerplants' in response.data
     
